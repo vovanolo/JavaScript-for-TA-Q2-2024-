59718368afd63400906210a4c88611181fa78aef
vovanolo <vpankiv@softserveinc.com>
2024-05-21 19:34:33 +0300

    topic1

---
+++ topic1.js
@@ -0,0 +1,66 @@
+// alert("hello from js file");
+
+// console.log("hello from the console"); // hello using console
+
+// here will go other code part
+
+// console.log();
+// console.log();
+// console.log();
+// console.log("Volodymyr");
+// console.log("Volodymyr");
+
+
+// let, const, var - old syntax
+
+// let name = " 'Volodymyr is good' ";
+// let age = 45;
+
+// name = "John";
+
+// name = age;
+
+// console.log(name);
+// console.log(age);
+
+// const NUMBER = 2000;
+
+// NUMBER = 3000;
+
+// console.log(NUMBER);
+
+// let if = 5;
+
+// let age = 20;
+
+// age = "text";
+
+// const user = {
+//     name: "Volodymyr",
+//     age: 45,
+//     isMarried: true,
+//     some: undefined
+// }
+
+// let age = 35;
+
+// console.log(typeof(age));
+
+// prompt("Whats your name?");
+
+// let name = prompt("Whats your name?");
+
+// console.log(name);
+
+// confirm("Are you ok?");
+
+// let ok = confirm("Are you ok?");
+
+// console.log(ok);
+
+// let name = prompt("whats your name");
+
+// console.log("Your name is " + name);
+// console.log("Volo" + "dymyr");
+
+// console.log(`Your name is ${name}`);
