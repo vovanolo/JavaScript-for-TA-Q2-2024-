930a19429ba43f86b545ae74d6f5c555c81c9ba3
vovanolo <vpankiv@softserveinc.com>
2024-06-13 19:43:03 +0300

    lesson-07

---
+++ topic6.js
@@ -0,0 +1,51 @@
+// getElementById
+
+// const title = document.getElementById("title");
+
+// console.log(typeof title);
+
+// const title = document.getElementsByClassName("title");
+
+// console.log(title);
+
+// const title = document.getElementsByTagName("h1");
+// console.log(title);
+
+// const title = document.querySelector("#title");
+
+// console.log(title);
+
+// const title = document.querySelectorAll("#title");
+
+// console.log(title);
+
+// let parent = document.querySelector(".parent");
+
+
+// console.log(parent);
+// console.log(parent.children);
+// console.log(parent.children[0]);
+// console.log(parent.firstElementChild);
+// console.log(parent.lastElementChild);
+
+// console.log(parent.children[parent.children.length - 1]);
+
+// console.log(parent.innerHTML);
+// parent.innerHTML = 'New text from JS';
+// parent.innerHTML = '<h1>New title from JS</h1>'
+
+// parent.innerHTML = `
+// <h1>New title from JS</h1>
+// <p>New parag from Js</p>
+// `
+
+// parent.setAttribute("id", "parent");
+// parent.id = "parent";
+
+// let parent = document.querySelector(".parent");
+
+// parent.style.backgroundColor = "green";
+// parent.style.color = "red";
+
+// parent.className = "newClass";
+// parent.classList.add("newClass");
---
+++ topic7.js
@@ -0,0 +1,52 @@
+
+// console.log(window.innerHeight);
+// console.log(window.innerWidth);
+
+
+// let newWindow = window.open("", "", "width=200, height=200");
+
+// newWindow.close();
+
+// function handleClick(){
+//     console.log("Click from Js");
+// }
+
+let title = document.querySelector("#title");
+let subtitle = document.querySelector("#subtitle");
+
+// title.onclick = function(){
+//     console.log("Click from Js");
+// }
+
+// function handleClick(){
+//     console.log("Click from Js event listener");
+// }
+
+// title.addEventListener("click", handleClick);
+
+// title.addEventListener("click", function(event){
+//     // console.log(event);
+//     // console.log(event.target);
+//     // console.log(event.type);
+//     // event.target.style.color = 'red';
+//     // subtitle.style.color = 'red';
+
+
+// });
+
+// let input = document.querySelector("#inp");
+// let button = document.querySelector("#btn");
+
+// button.addEventListener("click", function(){
+//     console.log(input.value);
+// })
+
+// let registerForm = document.querySelector("#registerForm");
+
+// registerForm.addEventListener("submit", function(event){
+//     event.preventDefault();
+//     // console.log("form was submitted");
+//     console.log(event.target.elements.name.value);
+//     console.log(event.target.elements.password.value);
+
+// })
