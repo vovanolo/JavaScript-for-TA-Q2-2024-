8ea5d066d99323c26140f480f664eff8c335cda4
Volodymyr I Pankiv <vpankiv@softserveinc.com>
2024-06-27 19:12:25 +0300

    topic11

---
+++ topic11.js
@@ -0,0 +1,24 @@
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((result) => result.json())
+//     // .then((result) => console.log(result[0].name));
+//     .then((result) => result.map(element => {console.log(element.name)}));
+
+
+
+// apis
+
+// async function getUsers(){
+//     let result = await fetch("https://jsonplaceholder.typicode.com/users");
+//     let data = await result.json();
+//     console.log(data);
+// }
+
+// getUsers();
+
+// localStorage.setItem("name", "Volodymyr");
+
+// console.log(localStorage.getItem("name"));
+
+// localStorage.removeItem("name");
+
+// localStorage.clear();
