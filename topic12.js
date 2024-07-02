1148b10e10d951b73f3cf2adde77bd6750278e84
Volodymyr I Pankiv <vpankiv@softserveinc.com>
2024-07-02 19:24:10 +0300

    topic12

---
+++ topic12.js
@@ -0,0 +1,21 @@
+// let test = "some information";
+// let test = "some Info info info";
+
+
+// let regExp = /info/;
+// let regExp = /info/i;
+// i g m
+
+
+// console.log(regExp.test(test));
+
+// console.log(test.match(/info/ig));
+
+// console.log("sfbsgdgcdwsfa".match(/[abc]/g));
+// console.log("sfbsgdgcdwsfa".match(/[a-f]/g));
+// console.log("sfbsgdgcdwsfa2njk1 kknk5 knk4".match(/[1-5]/g));
+// console.log("sfbsgdgcdwsfa2njk1 kknk5 knk4".match(/[1-5]/g));
+
+// console.log("sfdag 22323.232".match(/\d+\.?\d*/));
+
+
